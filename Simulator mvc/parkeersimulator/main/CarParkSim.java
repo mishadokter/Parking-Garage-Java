6fdda604b5cc38431ea36ebd71192e8a4ab98a5b
Dave Thijs <davethijs@hotmail.com>
2017-01-30 11:24:01 +0100

    Test voor misha

--- Simulator mvc/parkeersimulator/main/CarParkSim.java
+++ Simulator mvc/parkeersimulator/main/CarParkSim.java
@@ -43,31 +43,31 @@ public class CarParkSim {
         screen.getContentPane().add(timeView, BorderLayout.SOUTH);
         screen.getContentPane().add(statView, BorderLayout.EAST);
         screen.getContentPane().add(runController);
+        timeView.setBounds(350, 510, 200, 50);
         carParkView.setBounds(10, 10, 800, 500);
         runController.setBounds(0, 550, 800, 50);
         statView.setBounds(850,62,200,500);
         screen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         screen.setVisible(true);
     }
+
     /*
     * Overloading! jaja
     *
      */
-    public CarParkSim(TreeMap<String,String> optionFields) {
+    public CarParkSim(TreeMap<String, String> optionFields) {
 
         carParkModel = new CarParkModel();
         runController = new RunController(carParkModel);
         carParkView = new CarParkView(carParkModel);
         timeView = new TimeView(carParkModel);
 
-
         screen = new JFrame("CityPark Groningen parking simulator");
 
         JPanel panel = new JPanel();
-        panel.setBounds(0,0,850,650);
+        panel.setBounds(0, 0, 850, 650);
         Color panelColor = new Color(255, 10, 250, 70);
         panel.setBackground(panelColor);
-
 
 
         screen.setSize(850, 650);
@@ -77,17 +77,12 @@ public class CarParkSim {
         screen.getContentPane().add(timeView);
         screen.getContentPane().add(runController);
 
-        timeView.setBounds(350,510,200,50);
+        timeView.setBounds(350, 510, 200, 50);
         //timeView.setBackground(Color.DARK_GRAY);
         carParkView.setBounds(10, 10, 800, 500);
-        //carParkView.setBackground(Color.DARK_GRAY);
-        runController.setBounds(0, 550, 450, 50);
-        //runController.setBackground(Color.DARK_GRAY);
-        //screen.getContentPane().setBackground(Color.DARK_GRAY);
+        runController.setBounds(0, 550, 800, 50);
         screen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         screen.setVisible(true);
-        //screen.add(panel);
-
     }
 }
